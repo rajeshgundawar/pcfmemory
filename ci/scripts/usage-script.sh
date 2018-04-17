47cad481d6f190e6ed65cfe396c9c3b0cd51c60a
mujahidsoft <mujahidsoft@gmail.com>
2018-04-18 02:11:10 +0530

    add

--- ci/scripts/usage-script.sh
+++ ci/scripts/usage-script.sh
@@ -3,8 +3,8 @@ echo "Loginig into CF CLI"
 cf login -a $API_URL -u $USERNAME -p $PASSWORD -o $ORG -s $SPACE
 echo " Running cf usage report"
 cf usage-report >usage_report.txt
-mem_usage=$(cat usage_report.txt | grep $org| cut -d' ' -f5); echo "$SPACE Usage is $mem_usage"
-mem_quota=$(cat usage_report.txt | grep $org| cut -d' ' -f8); echo "$SPACE Quota is $mem_quota"
+mem_usage=$(cat usage_report.txt | grep $ORG| cut -d' ' -f5); echo "$SPACE Usage is $mem_usage"
+mem_quota=$(cat usage_report.txt | grep $ORG| cut -d' ' -f8); echo "$SPACE Quota is $mem_quota"
 percentage_usage=$((100*$mem_usage/$mem_quota));echo "Percentage of $SPACE  Usage is $percentage_usage%">email/body
 date
 echo " Printing Body of Email"
