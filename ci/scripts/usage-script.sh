55a40c780c6d5cb740c9b383e744c6f0bb46934e
mujahidsoft <mujahidsoft@gmail.com>
2018-04-21 01:20:06 +0530

    Add

--- ci/scripts/usage-script.sh
+++ ci/scripts/usage-script.sh
@@ -5,13 +5,18 @@ echo "Running cf usage report"
 cf usage-report >usage_report.txt
 mem_usage=$(cat usage_report.txt | grep $ORG| cut -d' ' -f5); echo "$SPACE Usage is $mem_usage"
 mem_quota=$(cat usage_report.txt | grep $ORG| cut -d' ' -f8); echo "$SPACE Quota is $mem_quota"
-percentage_usage=$((100*$mem_usage/$mem_quota));echo "Percentage of $SPACE  Usage is $percentage_usage%">email/body
-date
-echo  "Printing Body of Email"
-cat email/body
-echo "$ENV Memory Usage is above Threshold" >email/subject
-cat email/subject
-echo "$TO" >email/usr
-cat email/usr
-echo "End of script"
-
+percentage_usage=$((100*$mem_usage/$mem_quota))
+if [ "percentage_usage" -lt "90" ]
+then
+ exit 0;
+else
+ echo "Percentage of $SPACE  Usage is $percentage_usage%">email/body
+ date
+ echo  "Printing Body of Email"
+ cat email/body
+ echo "$ENV Memory Usage is above Threshold" >email/subject
+ cat email/subject
+ echo "$TO" >email/usr
+ cat email/usr
+ echo "End of script"
+fi;
