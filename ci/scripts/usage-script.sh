1e6bc8a7af9305b372d5500524d6e8da38e3072c
mujahidsoft <mujahidsoft@gmail.com>
2018-04-23 21:01:51 +0530

    Adding Threshold par and changing script accordingly

--- ci/scripts/usage-script.sh
+++ ci/scripts/usage-script.sh
@@ -6,7 +6,7 @@ cf usage-report >usage_report.txt
 mem_usage=$(cat usage_report.txt | grep $ORG| cut -d' ' -f5); echo "$SPACE Usage is $mem_usage"
 mem_quota=$(cat usage_report.txt | grep $ORG| cut -d' ' -f8); echo "$SPACE Quota is $mem_quota"
 percentage_usage=$((100*$mem_usage/$mem_quota)); echo "$percentage_usage% is usage of $SPACE"
-if [ "$percentage_usage" -lt "90" ]
+if [ "$percentage_usage" -lt "$THRESHOLD" ]
 then
  echo "Existing Memory Usage is $percentage_usage is below threshold"
  echo "End of Taski"
